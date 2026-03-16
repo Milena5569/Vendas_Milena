ed6b5a1fba04a5447528728b7d19ff7a1fe328cc
Milena5569 <cristiellemilena17@gmail.com>
2026-03-16 13:44:27 -0300

    fix: remove .next annd resolve dependency conflict

--- .next/dev/server/app/colecoes/page.js
+++ .next/dev/server/app/colecoes/page.js
@@ -1,5 +1,5 @@
 var R=require("../../chunks/ssr/[turbopack]_runtime.js")("server/app/colecoes/page.js")
-R.c("server/chunks/ssr/node_modules_5206a3f9._.js")
+R.c("server/chunks/ssr/node_modules_d0aadeeb._.js")
 R.c("server/chunks/ssr/[root-of-the-server]__6ebd602f._.js")
 R.c("server/chunks/ssr/node_modules_next_dist_7381059c._.js")
 R.c("server/chunks/ssr/[externals]__7f148858._.js")
--- .next/dev/server/chunks/ssr/[root-of-the-server]__7ecfb97a._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__7ecfb97a._.js
@@ -416,7 +416,7 @@ function AboutPage() {
                                             columnNumber: 15
                                         }, this),
                                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$client$2f$app$2d$dir$2f$link$2e$react$2d$server$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {
-                                            href: "/lojas#shopee",
+                                            href: "/lojas/shopee",
                                             className: "inline-flex items-center justify-center rounded-full border border-border-soft bg-surface-card/70 px-6 py-3 text-sm font-semibold text-text-secondary transition-colors hover:border-pink-300/40 hover:bg-surface-pink hover:text-text-primary",
                                             children: "Começar a descobrir ofertas"
                                         }, void 0, false, {
